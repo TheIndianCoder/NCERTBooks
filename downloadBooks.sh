bd7f46a00620f9c10436546649270c80ca462179
Amrit Preet Singh <32786653+TheIndianCoder@users.noreply.github.com>
2019-05-16 18:10:03 +0530

    my own

--- downloadBooks.sh
+++ downloadBooks.sh
@@ -4,8 +4,13 @@ while read line
 do
     dirname=`echo $line | cut -d "|" -f1`
     url=`echo $line | cut -d "|" -f2`
-    mkdir -p "$dirname"
-    pushd "$dirname"
-    wget $url
-    popd
+    # mkdir -p "$dirname"
+    # pushd "$dirname"
+    # wget $url
+    # popd
+    filename=${url##*/}
+    # echo $filename
+    # echo ncert-books/$dirname/$filename
+    gsutil cp $url gs://ncert-books/$dirname/$filename
 done < urls.txt
+
